b982f12fe74af1ca155da92919b7b434f702169e
Zulu <jzuluaga55@gmail.com>
2017-05-26 11:58:55 -0500

    Begin

---
+++ POOTaller/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace POOTaller
+{
+    public abstract class Product
+    {
+        public int Id { get; set; }
+
+        public string Description { get; set; }
+
+        public abstract decimal GetValue();
+
+        public override string ToString()
+        {
+            return string.Format("{0} {1}", Id, Description); 
+        }
+    }
+}
